d49e5befe6b716dd572dce297dd5caf6b184fa84
Max Gabrielsson <max@gabrielsson.com>
2023-04-26 14:35:05 +0200

    add feature overload

--- spatial/src/spatial/core/functions/scalar/st_asgeojson.cpp
+++ spatial/src/spatial/core/functions/scalar/st_asgeojson.cpp
@@ -1,6 +1,7 @@
 #include "duckdb/common/vector_operations/generic_executor.hpp"
 #include "duckdb/common/vector_operations/unary_executor.hpp"
 #include "duckdb/parser/parsed_data/create_scalar_function_info.hpp"
+#include "duckdb/planner/expression/bound_function_expression.hpp"
 
 #include "spatial/common.hpp"
 #include "spatial/core/functions/scalar.hpp"
@@ -136,9 +137,9 @@ static void ToGeoJSON(const Geometry &geom, yyjson_mut_doc* doc, yyjson_mut_val*
 }
 
 //------------------------------------------------------------------------------
-// GEOMETRY -> GEOJSON
+// GEOMETRY -> GEOJSON Fragment
 //------------------------------------------------------------------------------
-void GeometryToGeoJSONFunction(DataChunk &args, ExpressionState &state, Vector &result) {
+static void GeometryToGeoJSONFragmentFunction(DataChunk &args, ExpressionState &state, Vector &result) {
 	D_ASSERT(args.data.size() == 1);
 	auto &input = args.data[0];
 	auto count = args.size();
@@ -159,9 +160,152 @@ void GeometryToGeoJSONFunction(DataChunk &args, ExpressionState &state, Vector &
         auto json_data = yyjson_mut_write(doc, 0, &json_size);
         auto json_str = StringVector::AddString(result, json_data, json_size);
 
+        free((void*)json_data);
         yyjson_mut_doc_free(doc);
         return json_str;
 	});
+}
+
+//------------------------------------------------------------------------------
+// GEOMETRY -> GEOJSON Feature
+//------------------------------------------------------------------------------
+struct GeoJSONFeatureBindData : public FunctionData { 
+	child_list_t<LogicalType> properties;
+    GeoJSONFeatureBindData(child_list_t<LogicalType> properties) 
+        : FunctionData(), properties(std::move(properties)) {
+    }
+public:
+	unique_ptr<FunctionData> Copy() const override {
+		return make_unique<GeoJSONFeatureBindData>(properties);
+	}
+	bool Equals(const FunctionData &other_p) const override {
+		return true;
+	}
+};
+
+static unique_ptr<FunctionData> GeometryToGeoJSONFeatureBind(ClientContext &context, ScalarFunction &bound_function, vector<unique_ptr<Expression>> &arguments) {
+    if(arguments.size() != 2) {
+        throw BinderException("GeometryToGeoJSONFeature takes exactly two arguments");
+    }
+    child_list_t<LogicalType> properties;
+    if(arguments[1]->return_type.id() == LogicalTypeId::STRUCT) {
+        properties = StructType::GetChildTypes(arguments[1]->return_type);  
+    } else if(arguments[1]->return_type.id() == LogicalTypeId::SQLNULL) {
+        // do nothing, properties is empty
+    } else {
+        throw BinderException("GeometryToGeoJSONFeature takes a struct as second argument");
+    }
+    return make_unique<GeoJSONFeatureBindData>(std::move(properties));
+}
+
+
+class JSONAllocator {
+    // Stolen from the JSON extension :)
+public:
+	explicit JSONAllocator(ArenaAllocator &allocator)
+	    : allocator(allocator), yyjson_allocator({Allocate, Reallocate, Free, &allocator}) {
+	}
+
+	inline yyjson_alc *GetYYJSONAllocator() {
+		return &yyjson_allocator;
+	}
+
+	void Reset() {
+		allocator.Reset();
+	}
+
+private:
+	static inline void *Allocate(void *ctx, size_t size) {
+		auto alloc = (ArenaAllocator *)ctx;
+		return alloc->AllocateAligned(size);
+	}
+
+	static inline void *Reallocate(void *ctx, void *ptr, size_t old_size, size_t size) {
+		auto alloc = (ArenaAllocator *)ctx;
+		return alloc->ReallocateAligned((data_ptr_t)ptr, old_size, size);
+	}
+
+	static inline void Free(void *ctx, void *ptr) {
+		// NOP because ArenaAllocator can't free
+	}
+
+private:
+	ArenaAllocator &allocator;
+	yyjson_alc yyjson_allocator;
+};
+
+static void GeometryToGeoJSONFeatureFunction(DataChunk &args, ExpressionState &state, Vector &result) {
+
+    auto count = args.size();
+    auto &lstate = GeometryFunctionLocalState::ResetAndGet(state);
+    auto &func_expr = (BoundFunctionExpression&)state.expr;
+    auto &bind_data = (GeoJSONFeatureBindData&)*func_expr.bind_info;
+
+    auto &geom_vec = args.data[0];
+    auto &prop_vec = args.data[1];
+    geom_vec.Flatten(count);
+    prop_vec.Flatten(count);
+
+    bool has_properties = !bind_data.properties.empty();
+    
+    auto json_docs = (yyjson_mut_doc**)lstate.factory.allocator.AllocateAligned(count * sizeof(yyjson_mut_doc*));
+    auto json_objs = (yyjson_mut_val**)lstate.factory.allocator.AllocateAligned(count * sizeof(yyjson_mut_val*));
+    auto json_props = (yyjson_mut_val**)lstate.factory.allocator.AllocateAligned(count * sizeof(yyjson_mut_val*));
+
+    JSONAllocator json_allocator(lstate.factory.allocator);
+    
+    for(idx_t row_idx = 0; row_idx < count; row_idx++) {
+        auto geom_blob = FlatVector::GetData<string_t>(geom_vec)[row_idx];
+        auto geom = lstate.factory.Deserialize(geom_blob);
+
+        auto doc = yyjson_mut_doc_new(json_allocator.GetYYJSONAllocator());
+        auto obj = yyjson_mut_obj(doc);
+        yyjson_mut_doc_set_root(doc, obj);
+        json_docs[row_idx] = doc;
+        json_objs[row_idx] = obj;
+
+
+        yyjson_mut_obj_add_str(doc, obj, "type", "Feature");
+        auto geom_obj = yyjson_mut_obj(doc);
+        yyjson_mut_obj_add_val(doc, obj, "geometry", geom_obj);
+        ToGeoJSON(geom, doc, geom_obj);
+
+        if(has_properties) {
+            auto prop_obj = yyjson_mut_obj(doc);
+            yyjson_mut_obj_add_val(doc, obj, "properties", prop_obj);
+            json_props[row_idx] = prop_obj;
+        } else {
+            yyjson_mut_obj_add_null(doc, obj, "properties");
+            json_props[row_idx] = nullptr;
+        }
+    }
+    
+    child_list_t<LogicalType> schema;
+    if(has_properties) {
+        auto &prop_vecs = StructVector::GetEntries(prop_vec);
+        schema = StructType::GetChildTypes(prop_vec.GetType());
+        for(idx_t property_idx = 0; property_idx < prop_vecs.size(); property_idx++) {
+            auto &prop_vec = prop_vecs[property_idx];
+            auto &prop_name = schema[property_idx].first;
+            auto &prop_type = schema[property_idx].second;
+
+            for(idx_t row_idx = 0; row_idx < count; row_idx++) {
+                auto &prop_obj = json_props[row_idx];
+                yyjson_mut_obj_add_int(json_docs[row_idx], prop_obj, prop_name.c_str(), FlatVector::GetData<int64_t>(*prop_vec)[row_idx]);
+            }
+        }
+    }
+
+    for(idx_t row_idx = 0; row_idx < count; row_idx++) {
+        size_t json_size = 0;
+        // TODO: YYJSON_WRITE_PRETTY
+        auto json_data = yyjson_mut_write(json_docs[row_idx], 0, &json_size);
+        FlatVector::GetData<string_t>(result)[row_idx] = StringVector::AddString(result, json_data, json_size);
+    }
+
+    if(count == 1) {
+        result.SetVectorType(VectorType::CONSTANT_VECTOR);
+    }
 }
 
 //------------------------------------------------------------------------------
@@ -170,7 +314,13 @@ void GeometryToGeoJSONFunction(DataChunk &args, ExpressionState &state, Vector &
 void CoreScalarFunctions::RegisterStAsGeoJSON(ClientContext &context) {
 	auto &catalog = Catalog::GetSystemCatalog(context);
 
-	CreateScalarFunctionInfo info(ScalarFunction("ST_AsGeoJSON", {GeoTypes::GEOMETRY()}, LogicalType::VARCHAR, GeometryToGeoJSONFunction, nullptr, nullptr, nullptr, GeometryFunctionLocalState::Init));
+    ScalarFunctionSet geojson("ST_AsGeoJSON");
+    geojson.AddFunction(ScalarFunction({GeoTypes::GEOMETRY()}, LogicalType::VARCHAR, GeometryToGeoJSONFragmentFunction, 
+        nullptr, nullptr, nullptr, GeometryFunctionLocalState::Init));
+    geojson.AddFunction(ScalarFunction({GeoTypes::GEOMETRY(), LogicalTypeId::ANY}, LogicalType::VARCHAR, GeometryToGeoJSONFeatureFunction, 
+        GeometryToGeoJSONFeatureBind, nullptr, nullptr, GeometryFunctionLocalState::Init, LogicalTypeId::INVALID, FunctionSideEffects::NO_SIDE_EFFECTS, FunctionNullHandling::SPECIAL_HANDLING));
+
+	CreateScalarFunctionInfo info(geojson);
 	info.on_conflict = OnCreateConflict::ALTER_ON_CONFLICT;
 	catalog.CreateFunction(context, &info);
 }
