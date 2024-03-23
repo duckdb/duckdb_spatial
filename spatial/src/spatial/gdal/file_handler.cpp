da0f9bac16559a2e2619dc11f7123ac39446d519
Max Gabrielsson <max@gabrielsson.com>
2024-03-23 15:42:11 +0100

    attempt to handle chardev files, still doesnt work

--- spatial/src/spatial/gdal/file_handler.cpp
+++ spatial/src/spatial/gdal/file_handler.cpp
@@ -104,6 +104,15 @@ public:
 //--------------------------------------------------------------------------
 // GDAL DuckDB File system wrapper
 //--------------------------------------------------------------------------
+static bool IsStdCharDev(const char* file_name) {
+    return
+        !strcmp(file_name, "/dev/stdin") ||
+        !strcmp(file_name, "/dev/stdout") ||
+        !strcmp(file_name, "/dev/stderr") ||
+        !strcmp(file_name, "/dev/null") ||
+        !strcmp(file_name, "/dev/zero");
+}
+
 class DuckDBFileSystemHandler : public VSIFilesystemHandler {
 private:
 	string client_prefix;
@@ -121,6 +130,7 @@ public:
 	VSIVirtualHandle *Open(const char *prefixed_file_name, const char *access, bool bSetError,
 	                       CSLConstList /* papszOptions */) override {
 		auto file_name = StripPrefix(prefixed_file_name);
+        auto file_name_str = string(file_name);
 		auto &fs = FileSystem::GetFileSystem(context);
 
 		// TODO: Double check that this is correct
@@ -137,10 +147,7 @@ public:
 			}
 		} else if (access[0] == 'w') {
 			flags = FileFlags::FILE_FLAGS_WRITE;
-            if(fs.IsPipe(file_name)) {
-                // So we dont try to create a new /stdin/ or /stdout/ file and hit permission errors
-                flags |= FileFlags::FILE_FLAGS_FILE_CREATE;
-            } else {
+            if(!IsStdCharDev(file_name)) {
                 flags |= FileFlags::FILE_FLAGS_FILE_CREATE_NEW;
             }
 			if (len > 1 && access[1] == '+') {
@@ -164,11 +171,10 @@ public:
 		}
 
 		try {
-			string path(file_name);
 			// Check if the file is a directory
 
 #ifdef _WIN32
-			if (fs.DirectoryExists(path) && (flags & FileFlags::FILE_FLAGS_READ)) {
+			if (!FileSystem::IsRemoteFile(file_name) && fs.DirectoryExists(path) && (flags & FileFlags::FILE_FLAGS_READ)) {
 				// We can't open a directory for reading on windows without special flags
 				// so just open nul instead, gdal will reject it when it tries to read
 				auto file = fs.OpenFile("nul", flags);
@@ -184,12 +190,13 @@ public:
             }
   		} catch (std::exception &ex) {
             // Failed to open file via DuckDB File System. If this doesnt have a VSI prefix we can return an error here.
-			if (strncmp(file_name, "/vsi", 4) != 0) {
+			if (strncmp(file_name, "/vsi", 4) != 0 && !IsStdCharDev(file_name)) {
 				if (bSetError) {
 					VSIError(VSIE_FileError, "Failed to open file %s: %s", file_name, ex.what());
 				}
 				return nullptr;
 			}
+
 			// Fall back to GDAL instead
 			auto handler = VSIFileManager::GetHandler(file_name);
 			if (handler) {
@@ -209,12 +216,17 @@ public:
 
 		memset(pstatbuf, 0, sizeof(VSIStatBufL));
 
-		if (!(fs.FileExists(file_name) || fs.DirectoryExists(file_name))) {
+        if(IsStdCharDev(file_name)) {
+            pstatbuf->st_mode = S_IFCHR;
+            return 0;
+        }
+
+		if (!(fs.FileExists(file_name) || (!FileSystem::IsRemoteFile(file_name) && fs.DirectoryExists(file_name)))) {
 			return -1;
 		}
 
 #ifdef _WIN32
-		if (fs.DirectoryExists(file_name)) {
+		if (!FileSystem::IsRemoteFile(file_name) && fs.DirectoryExists(file_name)) {
 			pstatbuf->st_mode = S_IFDIR;
 			return 0;
 		}
